8e0788c004613c8a601c8ac2900eeac7f5c77e46
Kate <Riba0017@yandex.ru>
2020-10-15 23:28:56 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -38,9 +38,42 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let letters = [];
+    for(let i = 0; i < expr.length; i+= 10) {
+        letters.push(expr.substring(i, i+10))
+    }
+
+    let arr = letters.flat().filter(elem => elem !== '');
+
+    let words = [];
+    for(let i = 0; i < arr.length; i++) {
+        for(let j = 0; j < arr[i].length; j += 2) {
+            let char = arr[i].substring(j, j+2);
+            if(char === '00') words.push(' ');
+            if(char === '10') words.push('.');
+            if(char === '11') words.push('-');
+            if(char === '**') words.push('*');
+        }
+    }
+
+    words = words.join('');
+    let str = [];
+    for(let i = 0; i < words.length; i+= 5) {
+        str.push(words.substring(i, i+5).trim())
+    }
+
+    let final = [];
+    for(let k = 0; k < str.length; k++) {
+        if(str[k] === '*****') final.push(' ');
+        for(let prop in MORSE_TABLE) {
+            if(prop === str[k]) {
+                final.push(MORSE_TABLE[prop])
+            }
+        }
+    }
+    return final.join('');
 }
 
 module.exports = {
     decode
-}
+};
